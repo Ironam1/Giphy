8a083a939744ec805486d93501823db8b738c2d6
Ironam1 <ironam1@icloud.com>
2019-06-01 19:48:29 -0400

    should have committed a few more times. make new hero button properly generates new button. this button displays ten gifs, in still mode. when you click on the image, the gif animates. trying to make them stop moving on second click

---
+++ assets/javascript/logic.js
@@ -0,0 +1,72 @@
+
+function makeButton() {
+  $("#create").on("click", function(event) {
+    event.preventDefault();
+    var title = $("#name").val();
+    var newButton = $("<button/>", {
+      type: "button",
+      class: "super",
+      id: "giphy",
+      "text": title,
+      "data-person": title,
+    });
+    // console.log("name" + data);
+    $("#superheroes").append(newButton);
+  });
+}
+
+function runGif() {
+  $("body").on("click", ".super", function(event) {
+      event.preventDefault();
+    var person = $(this).attr("data-person");
+    // console.log("title " + person);
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      person +
+      "&api_key=c8HTVwDM8IuUwBZDWhQxgK3PxohfQ6pd&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var shownImage = $("<img>",{
+            class: "shown",
+            "src": results[i].images.fixed_height_still.url,
+            "data_still": results[i].images.fixed_height_still.url,
+            "data_animate": results[i].images.fixed_height.url,
+            "data_state": "still",
+        });
+       
+
+        gifDiv.prepend(p);
+        gifDiv.prepend(shownImage);
+        $("#hooray").prepend(gifDiv);
+      }
+    });
+  });
+}
+
+function animateGif() {
+    $("body").on("click", ".shown", function (event){
+        event.preventDefault();
+        var state= $(this).attr("data_state");
+        // console.log("STATE " + state);
+        if (state === "still") {
+            // alert("correct");
+            $(this).attr("src", $(this).attr("data_animate"));
+            $(this).attr("data-state", "animate");
+            console.log("state " + state);
+        } else {
+            $(this).attr("src", $(this).attr("data_still"));
+            $(this).attr("data-state", "still");
+        }
+        });
+    
+}
